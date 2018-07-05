f07916c0d9553fcc4e1269ffc7f60e79adc5d298
Bernard Ade <bernard@b-45.com>
2018-07-05 15:10:35 -0400

    build tailwind css with vue.js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div class="min-h-screen bg-grey-light p-8">
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+
+export default {
+  name: "app",
+  components: {
+    Hello
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Tailwind Vue</h1>
+  </div>
+</template>
+ 
+ 
