61505152524c096029c42baf7df8d7101d2561c3
b-45 <bernard11ade@gmail.com>
2018-09-06 11:52:48 -0400

    setup font awesome for icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimesCircle, faClipboardList } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTimesCircle, faClipboardList)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
